b3f7e43717fbc0a0c140300c23057fe9c88d2fee
sejalversee <sejalnaik0602@gmail.com>
2026-03-19 00:18:41 +0530

    Add files via upload
    I build simple calculator without loops or statements

---
+++ simple_calculator.py
@@ -0,0 +1,12 @@
+#simple calculator
+print("welcome to calculator")
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+add=a+b
+sub=a-b
+multi= a*b
+div=a/b
+print("addition of two numbers is",add)
+print("subtraction of two numbers is",sub)
+print("multiplication of two numbers is",multi)
+print("division of two numbers is",div)
