d05b3d64e808667637bbe29e79214669c78e9770
sejalversee <sejalnaik0602@gmail.com>
2026-03-19 00:08:05 +0530

    Add files via upload
    to find largest among 2 no

---
+++ largest_number.py
@@ -0,0 +1,7 @@
+# Find the largest of two numbers
+a = int(input("enter the first number value of a:"))
+b = int(input("enter the second number value of b:"))
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
